ee94f21ef46df03941ad28fdf5300b485c024d14
Tossapon <ntossapo@gmail.com>
2014-12-30 17:39:15 +0700

    first init

---
+++ ServerHandler.go
@@ -0,0 +1,72 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"log"
+	"strconv"
+	"encoding/json"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+type Accident struct{
+	Lat float64			`bson:"lat" json:"lat"`
+	Lng float64			`bson:"lng" json:"lng"`
+	Atype int64			`bson:"atype" json:"atype"`
+	Name string			`bson:"name" json:"name"`
+	Tel string			`bson:"tel" json:"tel"`
+	Desc string			`bson:"desc" json:"desc"`
+	DateTime string		`bson:"dateTime" json:"dateTime"`
+}
+
+func addAccidentPosition(w http.ResponseWriter, r *http.Request){
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 32)
+	lng, _ := strconv.ParseFloat(r.FormValue("lng"), 32)
+	aType, _ := strconv.ParseInt(r.FormValue("aType"),0 , 8)
+	name := r.FormValue("name")
+	tel := r.FormValue("tel")
+	desc := r.FormValue("desc")
+	dateTime := r.FormValue("dateTime")
+
+	session, err := mgo.Dial("localhost")
+	if err != nil{
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("patong").C("accident")
+	err = c.Insert(&Accident{lat, lng, aType, name, tel, desc, dateTime})
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Printf("add new accident \n{lat : %f, long : %f, aType : %d , name : \"%s\" , tel : \"%s\" , desc : \"%s\" dateTime : \"%s\"}", lat, lng, aType, name, tel, desc, dateTime)
+	bolResult, _ := json.Marshal(true)
+	fmt.Fprintf(w, "%s", string(bolResult))
+}
+
+func getAccidentPosition(w http.ResponseWriter, r *http.Request) {
+	var accident []Accident
+	session, err := mgo.Dial("localhost")
+	defer session.Close();
+
+	c := session.DB("patong").C("accident")
+	err = c.Find(bson.M{}).All(&accident)
+	if err != nil{
+		panic(err)
+	}
+	dataOut, _ := json.Marshal(accident)
+	fmt.Println("Get Data Called from : ")
+	fmt.Fprintf(w, "%s", string(dataOut))
+	
+}
+
+func main() {
+	fmt.Println("Server Start @ port", 8080)
+	http.HandleFunc("/add", addAccidentPosition)
+	http.HandleFunc("/get", getAccidentPosition)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}	
